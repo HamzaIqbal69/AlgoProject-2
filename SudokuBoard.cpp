1c1dde28aa2d8bc447c2b0b8039f7a1fe7e4e298
HamzaIqbal69 <hamza.n.iqbal@gmail.com>
2023-03-13 14:58:28 -0400

    Added helper functions and messed with given functions

--- SudokuBoard.cpp
+++ SudokuBoard.cpp
@@ -22,10 +22,16 @@ const int SquareSize = 3;	// Size of small squares
 *  	class SudokuBoard to solve sudoku puzzles
 *   The class is incomplete
 */
-class SudokuBoard {
+class SudokuBoard 
+{
   private:
 	int boardSize;			// Size of the board
 	matrix<int> sdkMatrix;	// Sudoku Matrix 
+	matrix<bool> row_conflicts;
+	matrix<bool> col_conflicts;
+	matrix<bool> sqr_conflicts;
+	int calcSquare(int row, int col);
+	void updateConflicts(int row, int col, int val);
 	
   public:
 	SudokuBoard(int N = 9);		// Constructor
@@ -40,42 +46,113 @@ SudokuBoard::SudokuBoard(int N) : boardSize(N)
 {	
 	// Resize conflict matricies
 	sdkMatrix.resize(N, N);
+	row_conflicts.resize(N,N);
+	col_conflicts.resize(N,N);
+	sqr_conflicts.resize(N,N);
 	clearBoard();
 }
+
+
 /* Read a Sudoku board from the input file. */
 void SudokuBoard::initializeBoard(ifstream &fin)
 {
    int digit;
    char ch;			// holds each value read from file
    clearBoard();	// clear the board first
+   int square;
+   // filling in sudoku board and inserting conflicts
    for (int i = 0; i < boardSize; i++)
    {
       for (int j = 0; j < boardSize; j++)
 	    {
-	       fin >> ch;
-          // If the read char is not Blank
-			if (ch != '.') {
+	        fin >> ch;
+            // If the read char is not Blank
+			if (ch != '.') 
+			{
 				digit = ch-'0';// Convert char to int
-				sdkMatrix[i][j] = digit; 
-		  }
+				sdkMatrix[i][j] = digit;
+				square = calcSquare(i, j);
+				row_conflicts[i][digit - 1] = 1;
+				col_conflicts[j][digit - 1] = 1;
+				sqr_conflicts[square][digit - 1] = 1;
+		  	}
         }
    }
 }
 
-/* Clear the puzzle by setting every cell to Blank = 0 */
+
+//Calculates current square based on indices
+int SudokuBoard::calcSquare(int row, int col)
+{
+	if(row < SquareSize && col < SquareSize)
+	{
+		return 0;
+	}
+	else if((col >= SquareSize) && (col < (2 * SquareSize)) && (row < SquareSize))
+	{
+		return 1;
+	}
+	else if((col >= SquareSize * 2) && (row < SquareSize))
+	{
+		return 2;
+	}
+	else if((col < SquareSize) && (row >= SquareSize) && (row < (2 * SquareSize)))
+	{
+		return 3;
+	}
+	else if((col >= SquareSize) && (col < (2 * SquareSize)) && (row >= SquareSize) && (row < (2 * SquareSize)))
+	{
+		return 4;
+	}
+	else if((col > SquareSize * 2) && (row >= SquareSize) && (row < (2 * SquareSize)))
+	{
+		return 5;
+	}
+	else if((col < SquareSize) && (row > (2 * SquareSize)))
+	{
+		return 6;
+	}
+	else if((col >= SquareSize) && (col < (2 * SquareSize)) && (row > (2 * SquareSize)))
+	{
+		return 7;
+	}
+	else if((col > SquareSize * 2) && (row > (2 * SquareSize)))
+	{
+		return 8;
+	}
+}
+
+
+/* Clear the puzzle by setting every cell to Blank = 0 and reset conflict matrices */
 void SudokuBoard::clearBoard()
 {
    for (int i = 0; i < boardSize; i++)
       for (int j = 0; j < boardSize; j++)
       {
-         sdkMatrix[i][j] = Blank;
+      	sdkMatrix[i][j] = Blank;
+		row_conflicts[i][j] = 0;
+		col_conflicts[i][j] = 0;
+		sqr_conflicts[i][j] = 0;
       }
 }
 
+
+// Updates the conflict vectors given the index and the inserted value
+void SudokuBoard::updateConflicts(int row, int col, int val)
+{
+	int sqr = calcSquare(row, col);
+	row_conflicts[row][val - 1] = 1;
+	col_conflicts[col][val - 1] = 1;
+	sqr_conflicts[sqr][val - 1] = 1;
+}
+
+
 /* Recursive function to solve the sudoku puzzle.  */
-bool SudokuBoard::solveSudoku() {
+bool SudokuBoard::solveSudoku() 
+{
 	//***** To be completed ************
 	// returns true if solution found, otherwise, false
+	
     return false;
 }
 
@@ -144,7 +221,7 @@ int main()
 		cout <<"\n*****  NEW SUDOKU PUZZLE... ******"  << endl;
 		sdk->printSudoku(); // print the board on the terminal
 
-		
+
 		// ********  Evaluate and print conflicts *****
 
 		// If found, print out the resulting solution and final conflicts
